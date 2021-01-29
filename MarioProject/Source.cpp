7a2b17c0b8df4cb74b472a1e26787c324dba0961
SOwen3373 <72083521+SOwen3373@users.noreply.github.com>
2021-01-29 13:17:09 +0000

    Finished TUT 2
    The working result of Tut2

--- MarioProject/Source.cpp
+++ MarioProject/Source.cpp
@@ -1,8 +1,56 @@
 #include "SDL.h"
 #include "SDL_image.h"
 #include "SDL_mixer.h"
+#include "constants.h"
+#include <iostream>
+using namespace std;
+
+//Globals
+SDL_Window* g_window = nullptr;
+
+//Function Prototypes
+bool InitSDL();
+void CloseSDL();
 
 int main(int argc, char* args[])
 {
+	//check if SDL is setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+	CloseSDL();
 	return 0;
+}
+
+bool InitSDL()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (g_window == nullptr)
+		{
+			//window failed to open
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+		else
+			return true;
+	}
+}
+
+void CloseSDL()
+{
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
---
+++ MarioProject/constants.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//screen dimensions
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 416
